4e0e2a10b9c15deee7660b25d8d9781cde75ae62
Kincses Dani <danikincs@gmail.com>
2017-02-06 11:49:23 +0100

    environment struct 4byte

--- tk-01-tw-morse/createenvironmentdata.c
+++ tk-01-tw-morse/createenvironmentdata.c
@@ -11,4 +11,17 @@
  * temp: magnitude of the temperature
  */
 EnvironmentData CreateEnvironmentData(float acc_x, float acc_y, float acc_z, signed int temp) {
+	int acc_x_int = (int) (acc_x*10);
+	int acc_y_int = (int) (acc_y*10);
+	int acc_z_int = (int) (acc_z*10);
+
+
+	EnvironmentData env = {acc_x, acc_y, acc_z, temp};
+	printf("accx size: %i \n", acc_x_int);
+	printf("accy size: %i \n", acc_y_int);
+	printf("accz size: %i \n", acc_z_int);
+	printf("temp size: %i \n", temp);
+
+	printf("sizeof the struct : %d \n", sizeof(env));
+	return env;
 }
